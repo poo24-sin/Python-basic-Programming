d5dd0923b06a6afe2ff37464d2b7ddeeb5d022dc
poo24-sin <69356697+poo24-sin@users.noreply.github.com>
2020-09-10 11:28:06 +0530

    Update Simple Interest.py

--- Simple Interest.py
+++ Simple Interest.py
@@ -2,19 +2,18 @@ def simple_interest(p,t,r):
     print('The principal amount is :',p)
     print('The time period is : ',t)
     print('The rate of interest is:',r)
+    global si
     si = (p*t*r)/100
     print('The simple interest is :' , si)
-
-
     return si
+
 principle = int(input("Enter Amount ."))
 time =  int(input("\nTime period."))
 rate =  float(input("\nRate of interest.."))
+simple_interest(principle,time,rate)
 
-print("Simple interest",simple_interest(principle,time,rate))
 
-simpl = principle*time*rate/100
-per_year = simpl/5
-print("Total :",per_year)
-per_month = per_year/12
-print("Every Moth",per_month)
+annually = si/5
+print("Annually:",annually)
+monthly = annually/12
+print("Monthly: ",monthly)
